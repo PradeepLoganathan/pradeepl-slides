ff40339fec2ddd1e9d58ee438e29bdb0e537ca03
Pradeep Loganathan <pradeep.loganathan@hotmail.com>
2024-02-27 11:13:36 +1000

    added home page and menu

--- generate-index.js
+++ generate-index.js
@@ -10,18 +10,45 @@ fs.readdir(presentationsDir, (err, folders) => {
   const links = folders.map(folder => `<li><a href="${folder}/index.html">${folder}</a></li>`).join('\n');
 
   const indexContent = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Presentations</title>
-    </head>
-    <body>
-        <h1>Available Presentations</h1>
-        <ul>
-            ${links}
-        </ul>
-    </body>
-    </html>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Blog</title>
+      <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header>
+          <h1>My Blog</h1>
+          <nav>
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </nav>
+      </header>
+  
+      <main>
+          <section class="blog-posts">
+              <article>
+                  <h2>Blog Post Title 1</h2>
+                  <p class="meta">Posted on January 1, 2024 by Author Name</p>
+                  <p>Short excerpt of the blog post goes here... </p>
+                  <a href="#" class="read-more">Read More</a>
+              </article>
+              </section>
+      </main>
+  
+      <footer>
+          <p>&copy; 2024 Your Name</p>
+      </footer>
+  
+      <script src="script.js"></script> 
+  </body>
+  </html>
+  
   `;
 
   fs.writeFile(outputIndexFile, indexContent, (err) => {
